79d170ca1d0e2f0f744b0203937f725cbfe6b945
Lucy Mair <l.mair@codat.io>
2023-07-10 11:55:50 +0100

    Generate component

---
+++ examples/angular/src/app/codat-link/codat-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-codat-link',
+  templateUrl: './codat-link.component.html',
+  styleUrls: ['./codat-link.component.css']
+})
+export class CodatLinkComponent {
+
+}
---
+++ examples/angular/src/app/codat-link/codat-link.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CodatLinkComponent } from './codat-link.component';
+
+
+
+@NgModule({
+  declarations: [
+    CodatLinkComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CodatLinkModule { }
