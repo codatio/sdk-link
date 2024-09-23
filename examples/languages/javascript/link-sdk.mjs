a9065bfb470d309565772573fdd5fb6915075173
RachelDavey <r.davey@codat.io>
2024-09-23 15:45:51 +0100

    Update javascript example

--- examples/languages/javascript/link-sdk.mjs
+++ examples/languages/javascript/link-sdk.mjs
@@ -4,6 +4,14 @@ let companyId = "";
 const linkSdkTarget = document.getElementById("target");
 const submitButton = document.getElementById("modal-button");
 const companyIdInput = document.getElementById("company-id-input");
+const linkingInstructions = document.getElementById("linking-instructions");
+const linkCompleted = document.getElementById("link-completed");
+const connectAgainButton = document.getElementById("connect-again");
+
+const toggleLinkCompletedDiv = (linkFinished) => {
+  linkingInstructions.style.display = linkFinished ? "none" : "block";
+  linkCompleted.style.display = linkFinished ? "block" : "none";
+};
 
 const closeCallback = () => {
   linkSdkTarget.style.pointerEvents = "none";
@@ -11,21 +19,24 @@ const closeCallback = () => {
 };
 
 const onClose = () => closeCallback();
+
 const onConnection = (connection) => {
   // Perform any logic here that should happen when a connection is linked
   console.log(`New connection linked with ID: ${connection.connectionId}`);
-}
+};
+
 const onFinish = () => {
   onClose();
-  // TODO conditionally render a success message
-}
+  toggleLinkCompletedDiv(true);
+};
+
 const onError = (error) => (error) => {
   // this error should be logged in your error tracking service
   console.error(`Codat Link SDK error`, error);
   if (!error.userRecoverable) {
     onClose();
   }
-}
+};
 
 const openModal = () => {
   linkSdkTarget.style.pointerEvents = "initial";
@@ -47,3 +58,4 @@ const handleTextInput = (e) => {
 
 companyIdInput.addEventListener("change", handleTextInput);
 submitButton.addEventListener("click", openModal);
+connectAgainButton.addEventListener("click", () => toggleLinkCompletedDiv(false));
